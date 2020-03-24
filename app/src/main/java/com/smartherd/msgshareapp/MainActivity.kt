212168aff734dd0023d838171dc054fbd7e48022
BaherAbdelfatah <baherbeho_ls@yahoo.com>
2020-03-24 20:34:51 +0200

    Send Data from One Activity to Another !

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.smartherd.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -14,9 +15,14 @@ class MainActivity : AppCompatActivity() {
 
 
         btnShowToast.setOnClickListener {
-
             Log.i("MainActivity","Button was clicked !")
             Toast.makeText(this,"Button was clicked !",Toast.LENGTH_SHORT).show()
+        }
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message:String = etUserMessage.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+            val intent= Intent(this,SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
