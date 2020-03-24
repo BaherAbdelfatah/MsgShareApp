0463a5000f7d2caf7cc5eafd7454dd5feb90155b
BaherAbdelfatah <baherbeho_ls@yahoo.com>
2020-03-24 18:37:12 +0200

    Handle Button Event !

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -2,11 +2,21 @@ package com.smartherd.msgshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnShowToast.setOnClickListener {
+
+            Log.i("MainActivity","Button was clicked !")
+            Toast.makeText(this,"Button was clicked !",Toast.LENGTH_SHORT).show()
+        }
     }
 }
