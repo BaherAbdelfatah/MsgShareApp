bdf3ec48a07114a39badf8df3762acd1b7c6fa96
BaherAbdelfatah <baherbeho_ls@yahoo.com>
2020-03-30 21:50:30 +0200

    Finalizing Material Design and Adding Arabic Language Support

--- app/src/main/java/com/smartherd/msgshareapp/activities/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/activities/MainActivity.kt
@@ -1,27 +1,33 @@
-package com.smartherd.msgshareapp
+package com.smartherd.msgshareapp.activities
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.smartherd.msgshareapp.Constants
+import com.smartherd.msgshareapp.R
+import com.smartherd.msgshareapp.showToast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object{
+        val TAG:String=MainActivity::class.java.simpleName
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         btnShowToast.setOnClickListener {
-            Log.i("MainActivity","Button was clicked !")
-            Toast.makeText(this,"Button was clicked !",Toast.LENGTH_SHORT).show()
+            Log.i(TAG,"Button was clicked !")
+            showToast(resources.getString(R.string.btn_was_clicked))
         }
         btnSendMsgToNextActivity.setOnClickListener {
             val message:String = etUserMessage.text.toString()
-            val intent= Intent(this,SecondActivity::class.java)
-            intent.putExtra("user_message",message)
+            val intent= Intent(this, SecondActivity::class.java)
+            intent.putExtra(Constants.USER_MSG_KEY,message)
             startActivity(intent)
 
         }
@@ -34,7 +40,8 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent.createChooser(intent,"Share to : "))
         }
         btnRecyclerViewDemo.setOnClickListener {
-            val intent =Intent(this,HobbiesActivity::class.java)
+            val intent =Intent(this,
+                HobbiesActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/smartherd/msgshareapp/adapters/HobbiesAdapter.kt
+++ app/src/main/java/com/smartherd/msgshareapp/adapters/HobbiesAdapter.kt
@@ -1,4 +1,4 @@
-package com.smartherd.msgshareapp
+package com.smartherd.msgshareapp.adapters
 import android.view.LayoutInflater
 import android.content.Context
 import android.content.Intent
@@ -6,10 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.smartherd.msgshareapp.models.Hobby
+import com.smartherd.msgshareapp.R
+import com.smartherd.msgshareapp.activities.MainActivity
+import com.smartherd.msgshareapp.showToast
 import kotlinx.android.synthetic.main.list_item.view.*
 
-class HobbiesAdapter(val context: Context, val hobbies: List<Hobby>) : RecyclerView.Adapter<HobbiesAdapter.MyViewHolder>(){
+class HobbiesAdapter(val context: Context, private val hobbies: List<Hobby>) : RecyclerView.Adapter<HobbiesAdapter.MyViewHolder>(){
 
+    companion object{
+        val TAG:String= HobbiesAdapter::class.java.simpleName
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.list_item,parent,false)
         return MyViewHolder(view)
@@ -29,23 +36,30 @@ class HobbiesAdapter(val context: Context, val hobbies: List<Hobby>) : RecyclerV
         var currentPosition: Int = 0
         init {
             itemView.setOnClickListener {
-                Toast.makeText(context, currentHobby!!.title + " Clicked !", Toast.LENGTH_SHORT).show()
+                currentHobby?.let {
+                    context.showToast(currentHobby!!.title + " Clicked !")
+
+                }
             }
 
             itemView.imgShare.setOnClickListener {
+                currentHobby?.let {
+                    val message: String = "My hobby is: " + currentHobby!!.title
 
-                val message: String = "My hobby is: " + currentHobby!!.title
+                    val intent = Intent()
+                    intent.action = Intent.ACTION_SEND
+                    intent.putExtra(Intent.EXTRA_TEXT, message)
+                    intent.type = "text/plain"
 
-                val intent = Intent()
-                intent.action = Intent.ACTION_SEND
-                intent.putExtra(Intent.EXTRA_TEXT, message)
-                intent.type = "text/plain"
-
-                context.startActivity(Intent.createChooser(intent, "Please select app: "))
+                    context.startActivity(Intent.createChooser(intent, "Please select app: "))
+                }
             }
         }
         fun setData(hobby: Hobby?, pos: Int) {
-            itemView.txvTitle.text = hobby!!.title
+
+            hobby?.let {
+                itemView.txvTitle.text = hobby.title
+            }
 
             this.currentHobby = hobby
             this.currentPosition = pos
